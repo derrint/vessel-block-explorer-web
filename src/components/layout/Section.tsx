9a653c85bdecd628f017280e15561ddb0008924d
Derrint <lorensioderrint@gmail.com>
2022-04-06 20:19:26 +0700

    Search Result : add empty state, fix search flow

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -5,13 +5,14 @@ type ISectionProps = {
   description?: string;
   yPadding?: string;
   children: ReactNode;
+  className?: string;
 };
 
 const Section = (props: ISectionProps) => (
   <div
     className={`max-w-screen-lg mx-auto px-3 ${
       props.yPadding ? props.yPadding : 'py-16'
-    }`}
+    } ${props.className || ''}`}
   >
     {(props.title || props.description) && (
       <div className="mb-12 text-center">
--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+
+import Link from 'next/link';
 import { FiSearch } from 'react-icons/fi';
 
 import { DropdownMenu } from '@components/dropdown';
@@ -5,48 +8,64 @@ import { DropdownMenu } from '@components/dropdown';
 const Search = () => {
   const filters = [
     {
-      id: 1,
+      id: 'all',
       label: 'All filters',
-      href: '/all',
     },
     {
-      id: 2,
+      id: 'txs',
       label: 'Transaction',
-      href: '/trx',
     },
     {
-      id: 3,
+      id: 'blocks',
       label: 'Block',
-      href: '/blocks',
     },
     {
-      id: 4,
+      id: 'address',
       label: 'Wallet Tx',
-      href: '/wallet',
     },
   ];
+
+  const [searchParams, setSearchParams] = React.useState({
+    q: '',
+    filter: 'all',
+  });
+
+  const { q, filter } = searchParams;
 
   return (
     <div className="flex justify-center items-center text-center relative">
       <div className="border-x border-y border-gray-200 bg-white px-5 h-14 rounded-2xl text-sm focus-within:border-primary flex justify-start items-center shadow-md">
         <DropdownMenu
-          title="All filters"
+          title={filters.find((x) => x.id === filter)?.label as any}
           items={filters}
-          onChange={(v: any) => console.log(v)}
+          onChange={(v: any) => {
+            setSearchParams({ ...searchParams, filter: v });
+          }}
         />
         <input
           className="focus:outline-none ml-3 sm:w-72 font-medium"
           type="search"
           name="search"
           placeholder="Search transaction, block, wallet (tx)"
+          onChange={(e) => {
+            setSearchParams({ ...searchParams, q: e.target.value });
+          }}
         />
       </div>
-      <button
-        type="submit"
-        className="flex items-center justify-center bg-gradient-to-r from-gradient-primary-start to-gradient-primary-end h-14 w-16 rounded-2xl ml-3 shadow-md text-white"
+      <Link
+        href={{
+          pathname: '/search',
+          query: { q, filter },
+        }}
+        passHref
       >
-        <FiSearch size={20} />
-      </button>
+        <button
+          type="submit"
+          className="flex items-center justify-center bg-gradient-to-r from-gradient-primary-start to-gradient-primary-end h-14 w-16 rounded-2xl ml-3 shadow-md text-white"
+        >
+          <FiSearch size={20} />
+        </button>
+      </Link>
     </div>
   );
 };
