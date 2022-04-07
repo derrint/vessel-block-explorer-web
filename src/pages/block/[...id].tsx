140ad40b283f0ed9cda3d2646d5853fb259d1e6f
Derrint <lorensioderrint@gmail.com>
2022-04-07 23:23:51 +0700

    Block Detail : update mobile view on table

--- src/pages/block/[...id].tsx
+++ src/pages/block/[...id].tsx
@@ -32,21 +32,23 @@ const BlockDetails = () => {
         </button>
       </div>
 
-      <div className="flex flex-col rounded-2xl shadow-md bg-white divide-y divide-gray-divider">
+      <div className="flex flex-col rounded-2xl shadow-md bg-white divide-y sm:divide-gray-divider">
         <div className="px-5 py-3">
           <h4 className="text-md font-bold">Overview</h4>
         </div>
-        <div className="px-5 py-3 grid grid-cols-4 gap-4 text-sm">
-          <div className="text-gray-text font-bold my-3">Block Height</div>
-          <div className="col-span-3 my-3">14337225</div>
-          <div className="text-gray-text font-bold mb-3">Timestamp</div>
-          <div className="col-span-3 mb-3">
+        <div className="px-5 py-5 sm:py-3 flex flex-col sm:grid sm:grid-cols-4 gap-2 sm:gap-4 text-sm">
+          <div className="text-gray-text font-bold sm:my-3">Block Height</div>
+          <div className="col-span-3 mb-3 sm:my-3 font-medium">14337225</div>
+          <div className="text-gray-text font-bold sm:mb-3">Timestamp</div>
+          <div className="col-span-3 mb-3 font-medium">
             4 hrs 16 mins ago (Mar-09-2022 12:11:18 PM +UTC)
           </div>
-          <div className="text-gray-text font-bold mb-3">Transaction</div>
-          <div className="col-span-3 mb-3">Total 536 transactions</div>
-          <div className="text-gray-text font-bold mb-3">Validator</div>
-          <div className="col-span-3 mb-3">
+          <div className="text-gray-text font-bold sm:mb-3">Transaction</div>
+          <div className="col-span-3 mb-3 font-medium">
+            Total 536 transactions
+          </div>
+          <div className="text-gray-text font-bold sm:mb-3">Validator</div>
+          <div className="col-span-3 mb-3 font-medium">
             <p>
               <Link href={`/address/${blockNumber}`} passHref>
                 <a className="text-primary inline break-all">{blockNumber}</a>
@@ -61,8 +63,8 @@ const BlockDetails = () => {
               />
             </p>
           </div>
-          <div className="text-gray-text font-bold mb-3">Gas Used</div>
-          <div className="col-span-3 mb-3">7,884,062 (20%)</div>
+          <div className="text-gray-text font-bold sm:mb-3">Gas Used</div>
+          <div className="col-span-3 sm:mb-3 font-medium">7,884,062 (20%)</div>
         </div>
       </div>
 
